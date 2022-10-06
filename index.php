7f5928024fdd3e288004ecea2d1a6e7e78f46265
Clara Garnes García <91025256+ClaraLG@users.noreply.github.com>
2022-10-06 17:56:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
         <title>Moc</title>
     
-        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <meta charset="utf-8">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
@@ -33,6 +33,8 @@
         <meta name="title" content="Moc - Movimientos culturales en la comunidad">
     
         <meta name="keywords" content="actividades culturales, formulario, comunidad">
+
+        <meta name=”robots” content="index, follow">
     
         <link rel="stylesheet" href="CSS\form_styles.css" type="text/css">
 
